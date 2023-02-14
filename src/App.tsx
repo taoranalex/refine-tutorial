d5521000b04edf805c998661ee4581ca42fbfbf1
alex <58145364@qq.com>
2023-02-14 12:38:47 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import React from "react";
-
 import { Refine } from "@pankod/refine-core";
-
-import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+import { HeadlessInferencer } from "@pankod/refine-inferencer/headless";
 
-function App() {
-  return (
-    <Refine
-      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-      routerProvider={routerProvider}
-    />
-  );
-}
-
+const App = () => {
+    return (
+        <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            resources={[
+                {
+                    name: "products",
+                    list: HeadlessInferencer,
+                    show: HeadlessInferencer,
+                    create: HeadlessInferencer,
+                    edit: HeadlessInferencer,
+                },
+            ]}
+        />
+    );
+};
 export default App;
